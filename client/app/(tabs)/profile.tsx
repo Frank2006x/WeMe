21068bf689d3f1ab6e4cca4d76104598d6d2a94c
Frank2006x <frank2006x@gmail.com>
2026-04-02 21:56:42 +0530

    feat: basic navigation and test api

---
+++ client/app/(tabs)/profile.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet } from "react-native";
+
+import { Collapsible } from "@/components/ui/collapsible";
+import { ExternalLink } from "@/components/external-link";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Fonts } from "@/constants/theme";
+
+export default function ProfileScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <IconSymbol
+          size={310}
+          color="#808080"
+          name="person.crop.circle.fill"
+          style={styles.headerImage}
+        />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}
+        >
+          Profile
+        </ThemedText>
+      </ThemedView>
+      <ThemedText>Manage your profile and settings.</ThemedText>
+      <Collapsible title="Profile features">
+        <ThemedText>
+          This tab is for profile management. You can edit your information,
+          view settings, and more.
+        </ThemedText>
+        <ExternalLink href="https://docs.expo.dev">
+          <ThemedText type="link">Learn more about Expo</ThemedText>
+        </ExternalLink>
+      </Collapsible>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
